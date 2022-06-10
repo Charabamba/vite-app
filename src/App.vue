99bf8a52781e3fd5714c725411c19d380c2de3ca
Charabamba <voralse@mail.ru>
2022-06-11 03:18:45 +0500

    add linting

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import ScoreBoard from "./components/ScoreBoard.vue";
-import { ref, computed } from "vue";
+import { computed } from "vue";
 import { useStore } from "vuex";
 const store = useStore();
 
 const score = computed(() => store.getters.getCurrentScore);
-const theme = computed(() => store.getters.getCurrentTheme);
 const incomeItems = computed(() => store.getters.getIncomeItems);
 const clickUpgrades = computed(() => store.getters.getClickUpgrades);
-function switchTheme() {
-  store.dispatch("switchTheme");
-}
 
 setInterval(() => {
   store.dispatch("updateScore");
@@ -30,26 +26,40 @@ function cickUpgrade(id) {
 
 <template>
   <ScoreBoard />
-  <button @click="handleClick">+1</button>
+  <button @click="handleClick">
+    +1
+  </button>
   <h2>Income shop:</h2>
-  <div v-for="item of incomeItems" :key="item.id">
-    <p>{{ item.name }} - {{ item.price }}</p>
+  <div
+    v-for="item of incomeItems"
+    :key="item.id"
+  >
+    <p>
+      {{ item.name }} - {{ item.price }}
+    </p>
     <button
       v-if="!item.purchased"
-      @click="buyIncome(item)"
       :disabled="score < item.price"
+      @click="buyIncome(item)"
     >
       +{{ item.income }}
     </button>
-    <p v-else>purchased</p>
+    <p v-else>
+      purchased
+    </p>
   </div>
   <h2>Click power upgrades:</h2>
-  <div v-for="item of clickUpgrades" :key="item.id">
-    <p>{{ item.name }}: {{ item.description }}</p>
+  <div
+    v-for="item of clickUpgrades"
+    :key="item.id"
+  >
+    <p>
+      {{ item.name }}: {{ item.description }}
+    </p>
     <button
       v-if="!item.purchased"
-      @click="cickUpgrade(item.id)"
       :disabled="score < item.price"
+      @click="cickUpgrade(item.id)"
     >
       purchase for {{ item.price }}
     </button>
