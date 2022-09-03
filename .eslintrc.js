8a6753d3562a107be4fe6fa3309717e66b9a4937
Charabamba <voralse@mail.ru>
2022-09-03 19:36:52 +0500

    add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,18 @@
 module.exports = {
+	parser: "vue-eslint-parser",
+	// https://github.com/vuejs/vue-eslint-parser#parseroptionsparser
+	parserOptions: {
+		parser: "@typescript-eslint/parser",
+	},
+
 	env: {
 		node: true,
 	},
-	extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:vue/vue3-recommended",
+	],
 	rules: {
 		"vue/no-unused-vars": "error",
 	},
